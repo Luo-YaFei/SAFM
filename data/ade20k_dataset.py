c7c74af4e9b56c7ea5324e790427313963183776
fly <492481530@qq.com>
2022-11-15 11:55:49 +1100

    spd 6

--- data/ade20k_dataset.py
+++ data/ade20k_dataset.py
@@ -10,7 +10,7 @@ import models.shape_context as sc
 import numpy as np
 from PIL import Image
 from data.base_dataset import BaseDataset, get_params, get_transform
-import util
+from util import util
 
 
 class ADE20KDataset(Pix2pixDataset):
